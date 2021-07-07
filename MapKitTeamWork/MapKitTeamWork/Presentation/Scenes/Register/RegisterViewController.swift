650f9994df8aec3a98cbfff05b075101944046ad
LizziChichuaa <83401753+LizziChichuaa@users.noreply.github.com>
2021-07-07 21:40:19 +0400

    small changes

--- MapKitTeamWork/MapKitTeamWork/Presentation/Scenes/Register/RegisterViewController.swift
+++ MapKitTeamWork/MapKitTeamWork/Presentation/Scenes/Register/RegisterViewController.swift
@@ -12,6 +12,7 @@ class RegisterViewController: BaseViewController {
     @IBOutlet weak var selectCountryBtn: UIButton!
     
     let transparentView = UIView()
+    let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,13 +20,34 @@ class RegisterViewController: BaseViewController {
         
     }
     
-    func addTransparentView() {
-        transparentView.frame = self.view.frame
-        self.view.addSubview(transparentView)
-        
-        transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+    func addTransparentView(frames: CGRect) {
+         transparentView.frame = self.view.frame
+         self.view.addSubview(transparentView)
+         
+         tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+         self.view.addSubview(tableView)
+         tableView.layer.cornerRadius = 5
+         
+         transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+         tableView.reloadData()
+         let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
+         transparentView.addGestureRecognizer(tapgesture)
+         transparentView.alpha = 0
+         UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+             self.transparentView.alpha = 0.5
+             self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height + 5, width: frames.width, height: 500)
+         }, completion: nil)
+     }
+    
+    @objc func removeTransparentView() {
+         let frames = selectCountryBtn.frame
+         UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+             self.transparentView.alpha = 0
+             self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+         }, completion: nil)
     }
 
     @IBAction func onSelectCountry(_ sender: Any) {
+        addTransparentView(frames: selectCountryBtn.frame)
     }
 }
