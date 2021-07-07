dc84986f357b94ac681a836febe42c09df159b1b
MacBook <>
2021-07-07 15:21:43 +0400

    - coordinates added

--- MapKitTeamWork/MapKitTeamWork/Domain/Model/CountryModel.swift
+++ MapKitTeamWork/MapKitTeamWork/Domain/Model/CountryModel.swift
@@ -10,8 +10,10 @@ import Foundation
 struct CountryModel: Codable{
     var country: String?
     var capital: String?
+    var coordinates: [Double]?
     
     enum CodingKeys: String, CodingKey {
+        case coordinates = "latlng"
         case country = "name"
         case capital = "capital"
     }
